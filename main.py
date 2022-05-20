5b231cf0755019f7e34648652e2bde0d50f32ce7
ImGishan <105406186+ImGishan@users.noreply.github.com>
2022-05-20 09:45:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -220,9 +220,9 @@ async def pm_media(bot, message):
     await bot.send_message(
         chat_id=-1001759991131,
         text=f"**Msg from:</b> {reference_id} **\n**Name:</b> [{message.from_user.first_name}](tg://user?id={message.from_user.id})**\n\n@admin"
+    )
     await message.forward(-1001788270921)
     
-    )
     reply_markup = BACK_BUTTONS
     text = "Ur Photo Sent To [ආතල් Ⓜ️🅴🅼🅴🆂😁🤩](https://t.me/memes_sinhala)"
     await message.reply_text(
