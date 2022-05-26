d6645472e1cf0004b9b3a0a73fc07e08ca31b100
Gishan Vithana <105406186+ImGishan@users.noreply.github.com>
2022-05-26 18:11:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -103,9 +103,10 @@ back = ReplyKeyboardMarkup(
         ],
         resize_keyboard=True  # Make the keyboard smaller
       )
-password = "9vQtrB1LxJbVXc5a"
+      
+password = "GG200695gg"
 DATABASE_URL=f'mongodb+srv://Gishankrishka1:GG200695gg@cluster0.42q73.mongodb.net/Gishankrishka1?retryWrites=true&w=majority'
-db = Database(DATABASE_URL, "Memehu_bott")
+db = Database(DATABASE_URL, "Memehub_bot")     
 #-------------------------------start---------------------------------------#
 
 @Client.on_message(filters.command("start"))
@@ -138,6 +139,17 @@ async def start(bot, message):
         disable_web_page_preview=True,
         quote=True
     )
+    if not await db.is_user_exist(message.from_user.id):
+       return
+    USER = InlineKeyboardMarkup([[              
+                 InlineKeyboardButton('USER', user_id=f"{message.from_user.id}")
+                 ]]
+                  )
+    info = await bot.get_users(user_ids=message.from_user.id)
+    USER_DETAILS = f"[{message.from_user.mention}](tg://user?id={message.from_user.id}) [`{message.from_user.id}`] Started Ur Bot.\n\n**First Name: `{info.first_name}`**\n**LastName: `{info.last_name}`**\n**Scam: `{info.is_scam}`**\n**Restricted: `{info.is_restricted}`**\n**Status:`{info.status}`**\n**Dc Id: `{info.dc_id}`**"
+    await bot.send_message(-1001759991131, text=USER_DETAILS, reply_markup=USER)
+    
+           
 
 
 
@@ -185,9 +197,7 @@ async def startprivate(bot, message):
 
 
 #----------------------------------main cmdd-------------------------------------#
-  
-    
-
+        
 @Client.on_message(filters.command(["help", "help@MemeHubTgSl_Bot"]))
 async def help(bot, message):
     if force_subchannel:
@@ -212,6 +222,76 @@ async def help(bot, message):
         reply_markup=CLOSE_BUTTON,
         disable_web_page_preview=True
          )
+
+@Client.on_message(filters.user(1884885842) & filters.private & filters.command("status"), group=5)
+async def status(bot, update):
+    if not await db.is_user_exist(update.from_user.id):
+         await db.add_user(update.from_user.id)
+         
+    await bot.send_sticker(update.chat.id, random.choice(STAT_STICKER))
+    total_users = await db.total_users_count()
+    text = "**Bot Advanced Statistics 📊**\n"
+    text += f"\n**Total Users:** `{total_users}`"
+
+    await update.reply_text(
+        text=text,
+        quote=True,
+        disable_web_page_preview=True
+    )
+
+@Client.on_message(
+    filters.private &
+    filters.command("broadcast") &
+    filters.user(1884885842) &
+    filters.reply
+)
+async def broadcast(bot, update, broadcast_ids={}):
+    
+    all_users = await db.get_all_users()
+    broadcast_msg= update.reply_to_message
+    while True:
+        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+        if not broadcast_ids.get(broadcast_id):
+            break
+
+    out = await update.reply_text(text=f"Broadcast Started! You will be notified with log file when all the users are notified.")
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    done = 0
+    failed = 0
+    success = 0
+    broadcast_ids[broadcast_id] = dict(total = total_users, current = done, failed = failed, success = success)
+        
+    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
+        async for user in all_users:
+            sts, msg = await send_msg(user_id = int(user['id']), message = broadcast_msg)
+            if msg is not None:
+                await broadcast_log_file.write(msg)
+            if sts == 200:
+                success += 1
+            else:
+                failed += 1
+            if sts == 400:
+                await db.delete_user(user['id'])
+            done += 1
+            if broadcast_ids.get(broadcast_id) is None:
+                break
+            else:
+                broadcast_ids[broadcast_id].update(dict(current = done, failed = failed, success = success))
+        
+    if broadcast_ids.get(broadcast_id):
+        broadcast_ids.pop(broadcast_id)
+    
+    completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
+    await asyncio.sleep(3)
+    await out.delete()
+
+    if failed == 0:
+        await update.reply_text(text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.", quote=True)
+    else:
+        await update.reply_document(document='broadcast.txt', caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.")
+        
+    os.remove('broadcast.txt') 
 
                        
 print("cmds.py Working....")
@@ -253,9 +333,6 @@ async def pm_text(bot, message):
     if message.from_user.id == 1884885842:
         await reply_text(bot, message)
         return
-    if message.from_user.id == 5347727010:
-        await reply_text(bot, message)
-        return
     info = await bot.get_users(user_ids=message.from_user.id)
     reference_id = int(message.chat.id)
     await bot.send_message(
@@ -270,9 +347,6 @@ async def pm_text(bot, message):
 @Client.on_message(filters.sticker & filters.private) 
 async def pm_media(bot, message):
     if message.from_user.id == 1884885842:
-        await replay_media(bot, message)
-        return
-    if message.from_user.id == 5347727010:
         await replay_media(bot, message)
         return
     info = await bot.get_users(user_ids=message.from_user.id)
@@ -319,9 +393,6 @@ async def pm_media(bot, message):
             ) 
             return
     if message.from_user.id == 1884885842:
-        await replay_media(bot, message)
-        return
-    if message.from_user.id == 5347727010:
         await replay_media(bot, message)
         return
     info = await bot.get_users(user_ids=message.from_user.id)
