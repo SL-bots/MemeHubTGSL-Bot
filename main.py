06940706d3563eeda8e23350dcafbe7a7ad15409
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-05-27 23:34:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -410,22 +410,6 @@ print("Cal Working....")
 
 @Client.on_message(filters.private & filters.text)
 async def pm_text(bot, message):
-    if force_subchannel:
-        try:
-            user = await bot.get_chat_member(force_subchannel, message.from_user.id)
-            if user.status == "kicked out":
-                await message.reply_text("Yourt Banned")
-                return 
-        except UserNotParticipant:
-            file_id = "CAADBQADOAcAAn_zKVSDCLfrLpxnhAI"
-            await bot.send_sticker(message.chat.id, file_id)
-            text = FORCESUB_TEXT
-            reply_markup = FORCESUB_BUTTONS
-            await message.reply_text(
-            text=text,
-            reply_markup=reply_markup
-            ) 
-            return
     if message.from_user.id == 1884885842:
         await reply_text(bot, message)
         return
@@ -433,82 +417,51 @@ async def pm_text(bot, message):
     reference_id = int(message.chat.id)
     await bot.send_message(
         chat_id=1884885842,
-        text=f"**Msg from:</b> {reference_id} **\n**Name:</b> {info.first_name}\n\n{message.text}**"
+        text=PM_TXT_ATT.format(reference_id, info.first_name, message.text)
     )
-    await bot.send_message(
-        chat_id=-1001759991131,
-        text=f"**Msg from:</b> {reference_id} **\n**Name:</b> {message.from_user.mention}\n\n{message.text}**"
-    )
-   
-@Client.on_message(filters.sticker & filters.private) 
+    
+
+@Client.on_message(filters.private & filters.sticker)
 async def pm_media(bot, message):
     if message.from_user.id == 1884885842:
         await replay_media(bot, message)
         return
     info = await bot.get_users(user_ids=message.from_user.id)
     reference_id = int(message.chat.id)
-    await message.forward(1884885842)
-    await bot.send_message(1884885842 ,f"Msg from:</b> {reference_id} **\n**Name:</b> {info.first_name}")
-    await message.forward(-1001759991131)
-    await bot.send_message(
-        chat_id=-1001759991131,
-        text=f"**Msg from:</b> {reference_id} **\n**Name:</b> [{message.from_user.first_name}](tg://user?id={message.from_user.id})**\n\n@admin"
+    await bot.copy_message(
+        chat_id=1884885842,
+        from_chat_id=message.chat.id,
+        message_id=message.id
     )
-    reference_id = True
-    if message.reply_to_message is not None:
-        file = message.reply_to_message
-        try:
-            reference_id = file.text.split()[2]
-        except Exception:
-            pass
-        try:
-            reference_id = file.caption.split()[2]
-        except Exception:
-            pass
-        await bot.send_message(
-            text=message.text,
-            chat_id=int(reference_id)
-        )
-   
-@Client.on_message(filters.media & filters.private)
+    await bot.send_message(1884885842, text=PM_TXT_ATTS.format(reference_id, info.first_name))
+    
+@Client.on_message(filters.private & filters.media)
 async def pm_media(bot, message):
-    if force_subchannel:
-        try:
-            user = await bot.get_chat_member(force_subchannel, message.from_user.id)
-            if user.status == "kicked out":
-                await message.reply_text("Yourt Banned")
-                return 
-        except UserNotParticipant:
-            file_id = "CAADBQADOAcAAn_zKVSDCLfrLpxnhAI"
-            await bot.send_sticker(message.chat.id, file_id)
-            text = FORCESUB_TEXT
-            reply_markup = FORCESUB_BUTTONS
-            await message.reply_text(
-            text=text,
-            reply_markup=reply_markup
-            ) 
-            return
     if message.from_user.id == 1884885842:
         await replay_media(bot, message)
         return
     info = await bot.get_users(user_ids=message.from_user.id)
     reference_id = int(message.chat.id)
-    await bot.send_message(1884885842 ,f"From:</b> {reference_id} **\n**Name:</b> {message.from_user.mention}")
-    await message.forward(-1001210985373)
-    await message.forward(-1001759991131)
-    await bot.send_message(
+    await bot.copy_message(
+        chat_id=1884885842,       
+        from_chat_id=message.chat.id,
+        message_id=message.id,
+        caption=PM_MED_ATT.format(reference_id, info.first_name)
+    )
+    reference_id = int(message.chat.id)
+    await bot.copy_message(
         chat_id=-1001759991131,
-        text=f"**Msg from:</b> {reference_id} **\n**Name:</b> [{message.from_user.first_name}](tg://user?id={message.from_user.id})**\n\n@admin"
-    )
-    reply_markup = BACK_BUTTONS
-    text = "Ur Photo Sent To [MemeHub Telegram 🇱🇰](https://t.me/memehubTGSL)"
-    await message.reply_text(
-        text=text,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        quote=True
-    )
-  
+        from_chat_id=message.chat.id,
+        message_id=message.id,
+        caption=PM_MED_ATT.format(reference_id, info.first_name),
+        reply_markup=InlineKeyboardMarkup([[
+                 InlineKeyboardButton("✅ᴀᴄᴄᴇᴘᴛ", callback_data="acce"),
+                 InlineKeyboardButton("❌ʀᴇᴊᴇᴄᴛ", callback_data="cloce")
+                 ]]
+                 )
+   )
+
+
 @Client.on_message(filters.text)
 async def reply_text(bot, message):
     reference_id = True
@@ -525,9 +478,10 @@ async def reply_text(bot, message):
         await bot.send_message(
             text=message.text,
             chat_id=int(reference_id)
-        ) 
-        
-@Client.on_message(filters.user(1884885842) & filters.media)
+        )
+
+
+@Client.on_message(filters.media)
 async def replay_media(bot, message):
     reference_id = True
     if message.reply_to_message is not None:
@@ -544,10 +498,37 @@ async def replay_media(bot, message):
             chat_id=int(reference_id),
             from_chat_id=message.chat.id,
             message_id=message.message_id
-        )             
-        
-@Client.on_message(filters.command("info"))
-async def user_info(bot, message):
+        )
+@Client.on_message(filters.private & filters.text)
+async def pm_text(bot, message):
+    if message.from_user.id == 1884885842:
+        await reply_text(bot, message)
+        return
+    info = await bot.get_users(user_ids=message.from_user.id)
+    reference_id = int(message.chat.id)
+    await bot.send_message(
+        chat_id=1884885842,
+        text=PM_TXT_ATT.format(reference_id, info.first_name, message.text)
+    )
+
+
+@Client.on_message(filters.private & filters.media)
+async def pm_media(bot, message):
+    if message.from_user.id == 1884885842:
+        await replay_media(bot, message)
+        return
+    info = await bot.get_users(user_ids=message.from_user.id)
+    reference_id = int(message.chat.id)
+    await bot.copy_message(
+        chat_id=1884885842,
+        from_chat_id=message.chat.id,
+        message_id=message.message_id,
+        caption=PM_MED_ATT.format(reference_id, info.first_name)
+    )
+
+
+@Client.on_message(filters.text)
+async def reply_text(bot, message):
     reference_id = True
     if message.reply_to_message is not None:
         file = message.reply_to_message
@@ -559,20 +540,30 @@ async def user_info(bot, message):
             reference_id = file.caption.split()[2]
         except Exception:
             pass
-    info = await bot.get_users(reference_id)
-    await message.delete()
-    await bot.send_message(
-        chat_id=OWNER_ID,
-        text=USER_DETAILS.format(
-            info.first_name,
-            info.last_name,
-            info.id, info.username,
-            info.is_scam,
-            info.is_restricted,
-            info.status,
-            info.dc_id
+        await bot.send_message(
+            text=message.text,
+            chat_id=int(reference_id)
         )
-    )
+
+
+@Client.on_message(filters.media)
+async def replay_media(bot, message):
+    reference_id = True
+    if message.reply_to_message is not None:
+        file = message.reply_to_message
+        try:
+            reference_id = file.text.split()[2]
+        except Exception:
+            pass
+        try:
+            reference_id = file.caption.split()[2]
+        except Exception:
+            pass
+        await bot.copy_message(
+            chat_id=int(reference_id),
+            from_chat_id=message.chat.id,
+            message_id=message.id
+        )
 print("Pm Working....")
 
 
@@ -640,7 +631,19 @@ async def tgm(bot, update):
         await update.answer(
              text="♻️ Updated ♻️",
         )
-        await update.message.edit(text="**♻️----Updated----♻️**", reply_markup=CLOSE_BUTTON)    
+        await update.message.edit(text="**♻️----Updated----♻️**", reply_markup=CLOSE_BUTTON)
+    elif update.data == "acce":
+        info = await bot.get_users(user_ids=update.message.from_user.id)
+        reference_id = int(update.message.chat.id)
+        await bot.copy_message(
+        chat_id=-1001210985373,
+        from_chat_id=-1001759991131,
+        message_id=update.message.id
+    )   
+        await update.answer(
+             text="✅ᴍᴇssᴀɢᴇ ᴀᴄᴄᴇᴘᴛᴇᴅ",
+        ) 
+    
 #--------------------------------------------------Inline------------------------------------------------#
 
 @Client.on_inline_query()
