729b15112329273eef3a778bad6d63538b5d186f
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-05-27 07:37:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -161,6 +161,25 @@ async def startprivate(bot, message):
 
 @Client.on_message(filters.regex(pattern="📊 Statistics"))   
 async def startprivate(bot, message):
+    splatform = platform.system()
+    platform_release = platform.release()
+    platform_version = platform.version()
+    architecture = platform.machine()
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(socket.gethostname())
+    mac_address = ":".join(re.findall("..", "%012x" % uuid.getnode()))
+    processor = platform.processor()
+    ram = humanbytes(round(psutil.virtual_memory().total))
+    cpu_freq = psutil.cpu_freq().current
+    if cpu_freq >= 1000:
+        cpu_freq = f"{round(cpu_freq / 1000, 2)}GHz"
+    else:
+        cpu_freq = f"{round(cpu_freq, 2)}MHz"
+    du = psutil.disk_usage(bot.workdir)
+    psutil.disk_io_counters()
+    disk = f"{humanbytes(du.used)} / {humanbytes(du.total)} " f"({du.percent}%)"
+    cpu_len = len(psutil.Process().cpu_affinity())
+    countb = await db.total_users_count()
     countb = await db.total_users_count()
     count = await bot.get_chat_members_count(-1001210985373)
     counta = await bot.get_chat_members_count(-1001759991131)
@@ -171,6 +190,21 @@ async def startprivate(bot, message):
  **MemeHub Telegram 🇱🇰  Users** : `{count}`
  **⚜️MemeHub Family⚜️ (Admins)**   : `{counta}`
  **ᴍᴇᴍᴇʜᴜʙ ᴏᴍғғɪᴄɪᴀʟ ʙᴏᴍᴛ 『🇱🇰』 Users** : `{countb}`
+◉──────────────────────────────────◉
+🖥 **System Information**
+
+**PlatForm :** `{splatform}`
+**PlatForm - Release :** `{platform_release}`
+**PlatFork - Version :** `{platform_version}`
+**Architecture :** `{architecture}`
+**Hostname :** `{hostname}`
+**IP :** `{ip_address}`
+**Mac :** `{mac_address}`
+**Processor :** `{processor}`
+**Ram : ** `{ram}`
+**CPU :** `{cpu_len}`
+**CPU FREQ :** `{cpu_freq}`
+**DISK :** `{disk}`
 ◉──────────────────────────────────◉
  """
  
