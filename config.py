bb1d0332d05f97c5c77f21e4f0d88df66200bb8b
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-05-27 09:17:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -172,35 +172,6 @@ DEV_BTN = InlineKeyboardMarkup([[
                  ]]
                   )
 
-CALCULATE_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton("DEL", callback_data="DEL"),
-        InlineKeyboardButton("AC", callback_data="AC"),
-        InlineKeyboardButton("(", callback_data="("),
-        InlineKeyboardButton(")", callback_data=")")
-        ],[
-        InlineKeyboardButton("7", callback_data="7"),
-        InlineKeyboardButton("8", callback_data="8"),
-        InlineKeyboardButton("9", callback_data="9"),
-        InlineKeyboardButton("÷", callback_data="/")
-        ],[
-        InlineKeyboardButton("4", callback_data="4"),
-        InlineKeyboardButton("5", callback_data="5"),
-        InlineKeyboardButton("6", callback_data="6"),
-        InlineKeyboardButton("×", callback_data="*")
-        ],[
-        InlineKeyboardButton("1", callback_data="1"),
-        InlineKeyboardButton("2", callback_data="2"),
-        InlineKeyboardButton("3", callback_data="3"),
-        InlineKeyboardButton("-", callback_data="-"),
-        ],[
-        InlineKeyboardButton(".", callback_data="."),
-        InlineKeyboardButton("0", callback_data="0"),
-        InlineKeyboardButton("=", callback_data="="),
-        InlineKeyboardButton("+", callback_data="+"),
-        ]]
-    )
-
 
 #Rndm Stkr
 
