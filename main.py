3bec4d2cc25c5efa84aaedd68d48d5eb820c3a14
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-05-30 09:35:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -660,12 +660,15 @@ async def tgm(bot, update):
             return
         info = await bot.get_users(user_ids=update.message.from_user.id)
         reference_id = int(update.message.chat.id)
-        await bot.copy_message(
+        process = await bot.copy_message(
         chat_id=-1001210985373,
         from_chat_id=-1001759991131,
         message_id=update.message.id,
         caption=f"{update.message.caption}\n\n<b>Accept By:</b>{update.from_user.mention}"
     )   
+        rid=update.message.caption.split()[2]
+        file_id="CAADBAADBwkAAmAw-FKQ4jfoM0moPwI"
+        await bot.send_sticker(rid, file_id)
         msg_id = process.id
         rid=update.message.caption.split()[2]
         await bot.send_message(rid, text=f"[ᴛʜɪs](https://t.me/memehubTGSL/{msg_id}) ᴘᴏsᴛ ᴀᴄᴄᴇᴘᴛᴇᴅ", disable_web_page_preview=True)
