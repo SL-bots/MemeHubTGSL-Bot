8e3a3e15bd22572e5f7ae73bda9570c934667c13
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-05-31 08:52:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -746,31 +746,6 @@ async def tgm(bot, update):
         await update.answer(
              text="✅ᴍᴇssᴀɢᴇ ᴀᴄᴄᴇᴘᴛᴇᴅ",
         )
-    elif update.data == "acce1":
-        if update.from_user.id not in AUTH_USERS:
-            await update.answer(
-                 text="U are Not Admin",
-            ) 
-            return
-        info = await bot.get_users(user_ids=update.message.from_user.id)
-        reference_id = int(update.message.chat.id)
-        process = await bot.copy_message(
-        chat_id=-1001210985373,
-        from_chat_id=-1001618208549,
-        message_id=update.message.id,
-        caption=f"{update.message.caption}\n\n<b>Accept By:</b>{update.from_user.mention}"
-    )
-        await update.message.delete()
-        rid=update.message.caption.split()[2]
-        file_id="CAADBAADBwkAAmAw-FKQ4jfoM0moPwI"
-        await bot.send_sticker(rid, file_id)
-        msg_id = process.id
-        rid=update.message.caption.split()[2]
-        await bot.send_message(rid, text=f"🎉 [ᴛʜɪs](https://t.me/memehubTGSL/{msg_id}) ᴘᴏsᴛ ᴀᴄᴄᴇᴘᴛᴇᴅ 🎉", disable_web_page_preview=True)
-        
-        await update.answer(
-             text="✅ᴍᴇssᴀɢᴇ ᴀᴄᴄᴇᴘᴛᴇᴅ",
-        )
     
 #--------------------------------------------------Inline------------------------------------------------#
 
