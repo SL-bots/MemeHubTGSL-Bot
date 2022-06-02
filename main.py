b7cfd6dcc286371101d209b4082e3786956dedea
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-06-02 18:08:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ async def startprivate(client, message):
         if -1001618208549:
             await client.send_message(
                 -1001618208549,
-                f"#NEWUSER: \n\n**User:** [{message.from_user.first_name}](tg://user?id={message.from_user.id})\n\**ID:**{message.from_user.id}\n Started @{BOT_USERNAME} !!",
+                f"#NEWUSER: \n\n**User:** [{message.from_user.first_name}](tg://user?id={message.from_user.id})\n**ID:**{message.from_user.id}\n Started @{BOT_USERNAME} !!",
             )
         else:
             logging.info(f"#NewUser :- Name : {message.from_user.first_name} ID : {message.from_user.id}")
